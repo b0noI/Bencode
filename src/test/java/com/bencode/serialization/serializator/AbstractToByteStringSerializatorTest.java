5c98b21b689a128420bd6ea35f82e70bb485db6c
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 21:31:48 +0000

    updating .gitignore with .idea/

---
+++ src/test/java/com/bencode/serialization/serializator/AbstractToByteStringSerializatorTest.java
@@ -0,0 +1,13 @@
+package com.bencode.serialization.serializator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbstractToByteStringSerializatorTest {
+
+    @Test
+    public void testSerialize() throws Exception {
+
+    }
+}
--- src/test/java/com/bencode/serialization/serializator/ByteSerializatorTest.java
+++ src/test/java/com/bencode/serialization/serializator/ByteSerializatorTest.java
@@ -1,11 +1,31 @@
 package com.bencode.serialization.serializator;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ByteSerializatorTest {
 
-    @org.junit.Test
+    @Test
     public void testConvertToBytes() throws Exception {
+        // input arguments
+        final byte inputValue = '?';
 
+        // mocks
+
+        // expected results
+        final byte[] expectedResult = new byte[]{'?'};
+
+        // creating test instance
+        final ByteSerializator testInstance = new ByteSerializator();
+
+        // execution test
+        final byte[] actaulResult = testInstance.convertToBytes(inputValue);
+
+        // result assert
+        assertArrayEquals(expectedResult, actaulResult);
+
+        // mocks verify
     }
+
 }
