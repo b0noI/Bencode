678989185886200181440598304897ce1fce8127
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 21:07:21 +0000

    making BencodeSerializable abstract class

--- src/main/java/com/bencode/serialization/BencodeSerializable.java
+++ src/main/java/com/bencode/serialization/BencodeSerializable.java
@@ -6,5 +6,5 @@ import java.io.Serializable;
  * any class that extends from this class will be Serialized in Bencode format
  * details about format can be found here: https://en.wikipedia.org/wiki/Bencode
  */
-public class BencodeSerializable implements Serializable {
+public abstract class BencodeSerializable implements Serializable {
 }
