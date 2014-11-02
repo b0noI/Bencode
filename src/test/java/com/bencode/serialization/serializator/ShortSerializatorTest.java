6f3706ff7cbdf6077ce31d9c26537201ae0d915d
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 21:43:05 +0000

    adding tests for ShortSerializator

--- src/test/java/com/bencode/serialization/serializator/ShortSerializatorTest.java
+++ src/test/java/com/bencode/serialization/serializator/ShortSerializatorTest.java
@@ -8,6 +8,24 @@ public class ShortSerializatorTest {
 
     @Test
     public void testConvertToBytes() throws Exception {
+        // input arguments
+        final short inputValue = 3000;
 
+        // mocks
+
+        // expected results
+        final byte[] expectedResult = new byte[]{11, -72};
+
+        // creating test instance
+        final ShortSerializator testInstance = new ShortSerializator();
+
+        // execution test
+        final byte[] actualResult = testInstance.convertToBytes(inputValue);
+
+        // result assert
+        assertArrayEquals(expectedResult, actualResult);
+
+        // mocks verify
     }
+
 }
