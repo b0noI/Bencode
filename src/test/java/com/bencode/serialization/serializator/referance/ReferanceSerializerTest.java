ea28eaece0e206dc2e5bb36e25577a868bd6089f
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 23:42:08 +0000

    adding initial test for PrimitiveFieldSerializator

--- src/test/java/com/bencode/serialization/serializator/referance/ReferanceSerializerTest.java
+++ src/test/java/com/bencode/serialization/serializator/referance/ReferanceSerializerTest.java
@@ -1,10 +1,36 @@
 package com.bencode.serialization.serializator.referance;
 
+import com.bencode.serialization.model.IBEncodeElement;
 import junit.framework.TestCase;
 
 public class ReferanceSerializerTest extends TestCase {
 
     public void testSerialize() throws Exception {
+        // input arguments
+        final TestClass inputForSerialization = new TestClass();
+
+        // mocks
+
+        // expected results
+
+        // creating test instance
+        final ReferanceSerializer testInstance = new ReferanceSerializer();
+
+        // execution test
+        final IBEncodeElement actualResult = testInstance.serialize(inputForSerialization);
+
+        // result assert
+        assertTrue(actualResult != null);
+
+        // mocks verify
+    }
+
+    private static class TestClass {
+
+        private short a = 2;
+
+        private int test24b = 22;
 
     }
+
 }
