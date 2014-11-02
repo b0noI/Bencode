fba95a2a57ac73795abdab1e258ae22680d21486
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 21:47:55 +0000

    setting language level to 8

---
+++ src/test/java/com/bencode/serialization/serializator/IntegerSerializatorTest.java
@@ -0,0 +1,13 @@
+package com.bencode.serialization.serializator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntegerSerializatorTest {
+
+    @Test
+    public void testConvertToBytes() throws Exception {
+
+    }
+}
