c3a60f539bca71554b8d38873a6cb615dfb02f7f
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 21:50:00 +0000

    adding IntegerSerializator

--- src/main/java/com/bencode/serialization/serializator/IntegerSerializator.java
+++ src/main/java/com/bencode/serialization/serializator/IntegerSerializator.java
@@ -1,7 +1,17 @@
 package com.bencode.serialization.serializator;
 
-/**
- * Created by b0noI on 02/11/14.
- */
-public class IntegerSerializator {
+
+import java.nio.ByteBuffer;
+
+class IntegerSerializator extends AbstractToByteStringSerializator<Integer> {
+
+    private static final int INT_SIZE_IN_BYTES = 4;
+
+    @Override
+    protected byte[] convertToBytes(final Integer instance) {
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(INT_SIZE_IN_BYTES);
+        byteBuffer.putInt(instance);
+        return byteBuffer.array();
+    }
+
 }
