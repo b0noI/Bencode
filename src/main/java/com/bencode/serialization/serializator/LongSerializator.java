f50f8550c146acf1e2d9ed39cef0c96da1c6996a
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 22:08:11 +0000

    adding LongSerializator

--- src/main/java/com/bencode/serialization/serializator/LongSerializator.java
+++ src/main/java/com/bencode/serialization/serializator/LongSerializator.java
@@ -1,7 +1,16 @@
 package com.bencode.serialization.serializator;
 
-/**
- * Created by b0noI on 02/11/14.
- */
-public class LongSerializator {
+import java.nio.ByteBuffer;
+
+class LongSerializator extends AbstractToByteStringSerializator<Long> {
+
+    private static final int LONG_SIZE_IN_BYTES = 8;
+
+    @Override
+    protected byte[] convertToBytes(final Long instance) {
+        final ByteBuffer byteBuffer = ByteBuffer.allocate(LONG_SIZE_IN_BYTES);
+        byteBuffer.putLong(instance);
+        return byteBuffer.array();
+    }
+
 }
