92127f2bb53575570da1e521738a3f8a617e4914
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 21:07:00 +0000

    adding basic BencodeSerializable class

---
+++ src/main/java/com/bencode/serialization/BencodeSerializable.java
@@ -0,0 +1,10 @@
+package com.bencode.serialization;
+
+import java.io.Serializable;
+
+/**
+ * any class that extends from this class will be Serialized in Bencode format
+ * details about format can be found here: https://en.wikipedia.org/wiki/Bencode
+ */
+public class BencodeSerializable implements Serializable {
+}
