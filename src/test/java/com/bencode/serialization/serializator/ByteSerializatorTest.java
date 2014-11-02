920aa09bd5578a2f7c7f8c26ed158986a601352f
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 21:27:07 +0000

    adding JUnit

---
+++ src/test/java/com/bencode/serialization/serializator/ByteSerializatorTest.java
@@ -0,0 +1,11 @@
+package com.bencode.serialization.serializator;
+
+import static org.junit.Assert.*;
+
+public class ByteSerializatorTest {
+
+    @org.junit.Test
+    public void testConvertToBytes() throws Exception {
+
+    }
+}
