9afce0610d034a638dc4fba8801a7232aa7136dc
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 22:10:31 +0000

    adding small comment

--- src/main/java/com/bencode/serialization/serializator/AbstractToByteStringSerializator.java
+++ src/main/java/com/bencode/serialization/serializator/AbstractToByteStringSerializator.java
@@ -2,6 +2,7 @@ package com.bencode.serialization.serializator;
 
 import com.sun.xml.internal.ws.encoding.soap.SerializationException;
 
+
 abstract class AbstractToByteStringSerializator<T> implements ISerializator<T> {
 
     private static final byte BYTE_STRING_SEPARATOR = ':';
@@ -10,6 +11,7 @@ abstract class AbstractToByteStringSerializator<T> implements ISerializator<T> {
     final public byte[] serialize(final T instance) {
         final byte[] bytes = convertToBytes(instance);
         final byte[] result = new byte[bytes.length + 2];
+        // this will work little bit more faster then ByteBuffer using
         System.arraycopy(bytes, 0, result, 2, bytes.length);
         if (bytes.length > Byte.MAX_VALUE) {
             throw new SerializationException("Instance length in bytes to long!");
