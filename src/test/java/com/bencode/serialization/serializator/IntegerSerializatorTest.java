9247d8b129a82131766ffba291d493f08238cc2c
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-02 21:50:22 +0000

    adding IntegerSerializatorTest

--- src/test/java/com/bencode/serialization/serializator/IntegerSerializatorTest.java
+++ src/test/java/com/bencode/serialization/serializator/IntegerSerializatorTest.java
@@ -8,6 +8,24 @@ public class IntegerSerializatorTest {
 
     @Test
     public void testConvertToBytes() throws Exception {
+        // input arguments
+        final int inputValue = 300_000_000;
 
+        // mocks
+
+        // expected results
+        final byte[] expectedResult = new byte[]{17, -31, -93, 0};
+
+        // creating test instance
+        final IntegerSerializator testInstance = new IntegerSerializator();
+
+        // execution test
+        final byte[] actualResult = testInstance.convertToBytes(inputValue);
+
+        // result assert
+        assertArrayEquals(expectedResult, actualResult);
+
+        // mocks verify
     }
+
 }
