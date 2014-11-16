e87dd78b7c25b1b1f5fc3dc18944209e347efbca
Viacheslav V Kovalevskyi <viacheslav@b0noi.com>
2014-11-16 23:05:47 +0000

    adding enw method to converter

--- src/main/java/com/bencode/deserializator/converter/IConverter.java
+++ src/main/java/com/bencode/deserializator/converter/IConverter.java
@@ -7,6 +7,10 @@ public interface IConverter<T extends IBEncodeElement> {
 
     public T convert(final byte[] bytes, final int position);
 
+    public default T convert(final byte[] bytes) {
+        return this.convert(bytes, 0);
+    }
+
     public static IConverter<IBEncodeElement> getConverter() {
         return RecursiveConverter.getInstance();
     }
